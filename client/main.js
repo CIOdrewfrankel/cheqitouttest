90fa12f77cdda50070559ca331abcb9c48aeb082
Morgan <morgan@cheqitoutinc.com>
2016-09-21 17:15:32 -0700

    First commit

---
+++ client/main.js
@@ -0,0 +1,79 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+questionThreeData = new Mongo.Collection("questionThreeData");
+allDataQuestions = new Mongo.Collection("allDataQuestions");
+
+Template.testAppMain.onCreated(function() {
+  sessionStart = new Date();
+  Session.set('finishTest', false);
+  allDataQuestions.insert(
+     {
+        startTime: sessionStart,
+     }
+  )
+  myFunction = function() {
+    sessionHour = new Date();
+    allDataQuestions.insert(
+       {
+          checkTime: sessionHour,
+       }
+    )
+  }
+  window.setTimeout(myFunction, 3600000);
+});
+
+Template.testAppMain.helpers({
+  'testFinish': function(){
+    if (Session.get('finishTest')) {
+      return "display: inline"
+    } else {
+      return "display: none"
+    }
+  },
+  'hideTest': function(){
+    if (Session.get('finishTest')) {
+      return "display: none"
+    } else {
+      return "display: inline"
+    }
+  }
+});
+
+Template.testAppMain.events({
+  'submit #questionOne'(event) {
+    alert('paused');
+    event.preventDefault();
+    questionOneValue = event.target.questionOneBox.value;
+    var contactsArray = [ {name: "Joe", area: "Vancouver, WA"},
+                         {name: "Jill", area: "Portland, OR" },
+                         {name: "Jose", area: "Salem, OR"}
+                       ];
+    //Return
+    window.eval(questionOneValue);
+    //Meteor.call("questionOneFunction", returnArray);
+    Meteor.call('questionOneFunction', returnArray, function(error, result){
+    if(error){
+        alert('Error');
+      } else{
+        alert('Success!')
+      }
+    });
+    //Meteor.call("aaa");
+  },
+  'submit #questionTwo'(event) {
+    event.preventDefault();
+    questionTwoValue = event.target.questionTwoBox.value;
+    window.eval(questionTwoValue);
+  },
+  'submit #questionThree'(event) {
+    event.preventDefault();
+    questionThreeValue = event.target.questionThreeBox.value;
+    window.eval(questionThreeValue);
+  },
+  'submit #finishTest'(event) {
+    event.preventDefault();
+    Session.set('finishTest', true);
+  },
+});
---
+++ server/main.js
@@ -0,0 +1,103 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+count = new Mongo.Collection("Count");
+questionThreeData = new Mongo.Collection("questionThreeData");
+allDataQuestions = new Mongo.Collection("allDataQuestions");
+
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
+
+Meteor.methods({
+  "aaa": function () {
+    Meteor.users.update({_id: Meteor.userId()},{$inc: {"profile.count": 1}});
+  },
+  "questionOneFunction": function(returnArray){
+    console.log('hello');
+    desiredArray = [
+      ['Joe', 'Jill', 'Jose'],
+      ['Vancouver, WA', 'Portland, OR', 'Salem, OR']
+    ];
+    if(returnArray.length !== desiredArray.length) {
+        console.log('Arrays are not the same length.');
+        console.log(returnArray.length);
+        console.log(desiredArray.length);
+        return false;
+    }
+    for (i = 0; i < returnArray.length; i++) {
+      for (j = 0; j < returnArray.length; j++) {
+        if(returnArray[i][j] !== desiredArray[i][j]) {
+            console.log('One of the arrays entries are not correct');
+            console.log(returnArray[i][j].toString());
+            return false;
+        }
+      }
+    }
+    console.log('worked');
+    var d = new Date();
+    allDataQuestions.insert(
+       {
+          question1: 'Correct',
+          date: d,
+       }
+    )
+  },
+  "questionTwoFunction": function(){
+    console.log('Question Two Function Called');
+    var d = new Date();
+    allDataQuestions.insert(
+       {
+          question2: 'Correct',
+          date: d,
+       }
+    )
+  },
+  "deleteEverything": function(){
+    console.log('Deleting...');
+    allDataQuestions.remove({});
+    questionThreeData.remove({});
+  }
+});
+
+// contactsArray = [ {name: "Joe", location: "Vancouver, WA"},
+//                   {name: "Jill", location: "Portland, OR" },
+//                   {name: "Jose", location: "Salem, OR"}
+//                 ];
+// returnArray = [[],[]];
+// for (i = 0; i < contactsArray.length; i++) {
+//       name = contactsArray[i].name;
+//       location = contactsArray[i].location;
+//       console.log(name);
+//       console.log(location);
+//       returnArray[0].push(name);
+//       returnArray[1].push(location);
+// }
+
+// questionThreeData.insert(
+//    {
+//       name: "Morgan",
+//       email: "test@test.com"
+//    }
+// )
+
+
+// myFunction = function() {
+  // console.log('doing my function');
+  // contactsArray = [ {name: "Joe", area: "Vancouver, WA"},
+  //                   {name: "Jill", area: "Portland, OR" },
+  //                   {name: "Jose", area: "Salem, OR"}
+  //                 ];
+  // returnArray = [[],[]];
+  // console.log(contactsArray.length)
+  // for (i = 0; i < contactsArray.length; i++) {
+  //       name = contactsArray[i].name;
+  //       area = contactsArray[i].area;
+  //       console.log(name);
+  //       console.log(area);
+  //       returnArray[0].push(name);
+  //       returnArray[1].push(area);
+  // }
+//}
+//myFunction();
